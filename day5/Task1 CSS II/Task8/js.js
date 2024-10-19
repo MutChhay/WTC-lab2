fd783b2582925ecb18128265cba2ce96b2141d30
MutChhay <mrchhay29@gmail.com>
2024-10-19 11:45:22 +0700

    new Update

---
+++ day5/Task1 CSS II/Task8/js.js
@@ -0,0 +1,8 @@
+// script.js
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    body.classList.toggle('light-theme');
+});
